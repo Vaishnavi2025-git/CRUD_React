c7a9541e1884d26bd060e24054a9ef7a076115b0
Vaishnavi2025-git <Vaishnavi.Dhulipala@cognizant.com>
2025-06-20 11:12:55 +0530

    Delete src directory

--- src/main/java/com/cts/repository/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cts.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cts.entity.Todo;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {}
-
